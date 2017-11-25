a95c5ae4b5cc63474206d3478decf9fce536a8b6
Dharma Sanjyal <rajsan44@gmail.com>
2017-11-24 19:19:37 -0800

    managed to display only the div

--- js/main.js
+++ js/main.js
@@ -24,51 +24,62 @@ var people = [
 ];
 function submitForm(){
 	//when submit is clicked we want to save the user input
-	var userInput = document.getElementById('username').value;
-	var password = document.getElementById('password').value;
+	var userInputs = document.getElementById('username').value;
+	var passwords = document.getElementById('password').value;
 
 	//Using for loop to 
-	for(i= 0; i < people.length; i++) {
-		if(userInput === people[i].username && password === people[i].password) {
-			console.log(userInput + " is logged in");
-			document.body.removeChild(document.getElementById('forms'));
+	for(i= 0; i < people.length; i++){
+		if(userInputs == people[i].username && passwords == people[i].password) {
+			console.log(userInputs + " is logged in");
+			//;
            document.write("Welcome to our Website");
-           return;
+          return; 
 		} else {
 			
+      document.body.removeChild(document.getElementById('forms'));
 			 var newDiv = document.createElement('div');
               var form = document.createElement('form');
+
            var label = document.createElement('label');
               label.textContent="NewUser: ";
                     var input = document.createElement('input');
                           input.type="text";
                            input.id="newUser";
-                           var newBtn =  document.createElement('button');
-		      newBtn.className ="btn btn-large btn-default";
+
+              var label1 = document.createElement('label1');
+              label1.textContent="Password: ";
+                    var input1 = document.createElement('input');
+                          input1.type="text";
+                           input1.id="newPassword";
+
+             var newBtn =  document.createElement('button');
+		          newBtn.className ="btn btn-large btn-default";
                 newBtn.textContent = "Register Now";
                      newBtn.id = "registerBtn";
                  label.appendChild(input);
+                 label1.appendChild(input1);
                   form.appendChild(label);
+                  form.appendChild(label1);
                 newDiv.appendChild(form);
                 newDiv.appendChild(newBtn);
-             document.body.appendChild(newDiv);
-             
-           // document.body.appendChild(newBtn);
-           //  document.body.appendChild(newDiv);
+             document.getElementById('info').appendChild(newDiv);
       console.log("you are not in the system");
+      alert("Register Below");
+      return;
+     // document.getElementById('forms').reset();
 };
+
 };
+ newBtn.addEventListener('click', newBtnFunction,);
+ //function newBtnFunction() {
+ 	//document.getElementById('form2').style.display="block";
+ 	//console.log("lets get started");
+ //};
 };
-/*
-//document.body.appendChild(document.getElementById('form2'))
- //newBtn.addEventListener('click', newBtnFunction, false);
- function newBtnFunction() {
- 	document.getElementById('form2').style.display="block";
- 	console.log("lets get started");
- };
-};
-return;
+function newBtnFunction(click){
+  document.write("Welcome!");
 };
 
 
-}*/
+
+
