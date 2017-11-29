b1c068db972eeecf2cc88f017dcb01730c0026ef
Dharma Sanjyal <rajsan44@gmail.com>
2017-11-29 11:33:21 -0800

    changed the whole js file

--- js/main.js
+++ js/main.js
@@ -1,83 +1,46 @@
-var people = [
+//creating an object array
+var members = [
 {
-   username: "dharma",
-  password : "Password0"
-},
-{
-	username: "alex",
-	password: "Password1"
+  username: "matt",
+  password: "Password1"
 },
 {
-	username: "chris",
-	password: "Password2"
-},
-{   username: "nick",
-	password: "Password3"
+  username: "chris",
+  password: "Password2"
 },
 {
-	username: "courtney",
-	password: "Password4"
+  username: "sam",
+  password: "Password3"
 },
-{   username: "sam",
-	password: "Password5"
+{
+  username: "courtney",
+  password: "Password4"
+},
+{
+  username: "lisa",
+  password: "Password5"
 }
 ];
-function submitForm(){
-	//when submit is clicked we want to save the user input
-	var userInput = document.getElementById('username').value;
-	var password = document.getElementById('password').value;
-    //Using for loop to loop through the object array
-  for(i = 0; i < people.length; i++){
-   console.log(people[i]);
- 
-	   //giving a condition to match the username and password
-		if(userInput === people[i].username && password === people[i].password) {
-			console.log(userInput + " is logged in");
-           document.write("Welcome to our Website");     
-		}  
-    //remove the log in form and display the registration form
-    else {	
- document.body.removeChild(document.getElementById('forms'));
-  var newDiv = document.createElement('div');
-              var form = document.createElement('form');
-           var label = document.createElement('label');
-              label.textContent="NewUser: ";
-             var input = document.createElement('input');
-                          input.type="text";
-                           input.id="newUser";
-           var label1 = document.createElement('label1');
-              label1.textContent="Password: ";
-            var input1 = document.createElement('input');
-                          input1.type="text";
-                           input1.id="newPassword";
-          //creating a new register button
-             var newBtn =  document.createElement('button');
-		          newBtn.className ="btn btn-large btn-primary";
-                newBtn.textContent = "Register Now";
-                     newBtn.id = "registerBtn";
-                 label.appendChild(input);
-                 label1.appendChild(input1);
-                  form.appendChild(label);
-                  form.appendChild(label1);
-                newDiv.appendChild(form);
-                newDiv.appendChild(newBtn);
-             document.getElementById('info').appendChild(newDiv);
-             //document.body.appendChild(newDiv);
-      console.log("you are not in the system");
-      alert("Register Below");
-    
-     // document.getElementById('forms').reset();
-};
-};
-
-// newBtn.addEventListener('click', newBtnFunction,);
- //function newBtnFunction() {
- 	//document.getElementById('form2').style.display="block";
- 	//console.log("lets get started");
- 
-//function newBtnFunction(){
-  //document.write("Welcome!");
-
-
-
-
+ //calling a function to save the userinput and run a for loop with some conditions
+ function submitForm(){
+  //saving the userInput values on click of log in button
+  var userName = document.getElementById('username').value;
+  var userPassword = document.getElementById('password').value;
+   //
+  //lets run a for loop to loop through the object array
+  for(var i = 0; i < members.length; i++){
+    //displaying the property and value of object array in console
+   // console.log(members[i]);
+    //lets use if/else statement to compare the userinput with the existing data
+    if(userName == members[i].username && userPassword == members[i].password){
+      console.log(userName);
+      console.log(userPassword);
+      //remove the form and display"Welcome to the website"
+      document.write("Welcome to Our Website");
+      //return statement stops the loop but i am not sure whats happening here;
+      return;
+    } else {
+         var newDiv = document.createElement('div');
+    }
+  }
+ }
